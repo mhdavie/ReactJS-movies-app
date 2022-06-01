8246ea3acefd3f267acd7d71a0407797f51d7165
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-06-01 10:29:34 -0600

    added more data

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,16 +5,70 @@ function Home() {
     const movies = [
 
         {
+            id:1,
             title: "Spider Man",
             image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
         },
         
         {
+            id:2,
             title: "Batman",
             image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
         },
         
         {
+            id:3,
+            title: "IronMan",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        {
+            id:1,
+            title: "Spider Man",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        
+        {
+            id:2,
+            title: "Batman",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        
+        {
+            id:3,
+            title: "IronMan",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        {
+            id:1,
+            title: "Spider Man",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        
+        {
+            id:2,
+            title: "Batman",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        
+        {
+            id:3,
+            title: "IronMan",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        {
+            id:1,
+            title: "Spider Man",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        
+        {
+            id:2,
+            title: "Batman",
+            image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
+        },
+        
+        {
+            id:3,
             title: "IronMan",
             image_url: "https://pbs.twimg.com/media/FR7DvJFVIAAuoH6?format=jpg&name=large"
         }
@@ -27,7 +81,7 @@ function Home() {
 
                 {movies.map(movie => {
                     return (
-                        <Movie>
+                        <Movie key= {movie.id}>
                     
                             <a href="http://google.com"> <img src={movie.image_url} alt={movie.title}/></a>
                             <span>{movie.title}</span>
